f552dd6c42841af43cdff14b06e561c97266f27b
moin-Jana <172497896+moin-Jana@users.noreply.github.com>
2025-04-04 17:17:41 +0200

    DEV: use correct setting name

--- javascripts/discourse/api-initializers/update-group-setting-banner.js
+++ javascripts/discourse/api-initializers/update-group-setting-banner.js
@@ -6,7 +6,7 @@ export default apiInitializer((api) => {
   if (currentUser?.admin) {
     if (settings.show_groups_deprecated?.trim().length > 0) {
       api.addGlobalNotice(
-        "The Prefilled Composer Link Generator theme component has been updated to support a group dropdown in the settings. You are still using the old setting, which will be removed in a future update. Please migrate the groups to the new show_group_ids setting. This warning will continue to appear until all groups are removed from the old setting.",
+        "The Prefilled Composer Link Generator theme component has been updated to support a group dropdown in the settings. You are still using the old setting, which will be removed in a future update. Please migrate the groups to the new restrict_to_groups setting. This warning will continue to appear until all groups are removed from the old setting.",
         "prefilled-composer-generator-setting-update",
         {
           dismissable: true,
